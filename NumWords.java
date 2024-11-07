87fa5fb5d9fb5a29d6e3ead3bf2e5783b240d898
Gallind <gal.lind@post.runi.ac.il>
2024-11-07 08:11:56 +0200

    Perfected NumWords

--- NumWords.java
+++ NumWords.java
@@ -7,9 +7,10 @@ public class NumWords {
 		int Hs, Ts, Os;
 		Os = FullNum % 10;
 		Ts = (int)(Math.floor(FullNum/10)%10);
-		Hs = (int)(Math.floor(FullNum/100)%10);
+		Hs = (int)(Math.floor(FullNum/100));
+
 		
-		if (FullNum==2024){Hs=20;}
+		//if (FullNum==2024){Hs=20;}
 
 		System.out.println(Hs + " hundreds, " + Ts + " tens, and " + Os + " ones.");
 	}
